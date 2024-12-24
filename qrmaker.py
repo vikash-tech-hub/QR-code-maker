b77079dd1094169ba4b1ad2792d17707e034a67e
vikash-tech-hub <vtech076@gmail.com>
2024-12-24 11:15:22 +0530

    Add files via upload

---
+++ qrmaker.py
@@ -0,0 +1,27 @@
+import qrcode
+
+# Taking UPI ID as input
+upi_id = input("Enter your UPI ID: ")
+
+# Define UPI URLs based on the UPI ID and payment apps
+
+
+
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+# Create QR codes for each payment app
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+# Save the QR codes to image files
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+# Display the QR codes
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
